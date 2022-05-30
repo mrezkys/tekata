87ba6169656bf728ac29bb4ae23346ad476dd04f
mrezkys <mrezkysulihin@gmail.com>
2022-05-30 17:21:20 +0800

    feat: main

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:tekata/app/controllers/loading_controller.dart';
 
 import 'app/routes/app_pages.dart';
 
-void main() {
+void main() async {
+  await GetStorage.init();
+  Get.put(LoadingController());
   runApp(
     GetMaterialApp(
       title: "Application",
       initialRoute: AppPages.INITIAL,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        fontFamily: 'poppins',
+      ),
       getPages: AppPages.routes,
     ),
   );
