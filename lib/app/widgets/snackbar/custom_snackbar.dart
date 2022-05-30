3b4d4601fc2a3bcbc73b626f32b1a07054575d8a
mrezkys <mrezkysulihin@gmail.com>
2022-05-30 17:21:55 +0800

    feat: custom snackbar

---
+++ lib/app/widgets/snackbar/custom_snackbar.dart
@@ -0,0 +1,138 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
+import 'package:tekata/app/style/app_color.dart';
+
+class CustomSnackbar {
+  static errorSnackbar(String? title, String? message) {
+    Get.rawSnackbar(
+      duration: Duration(seconds: 4),
+      dismissDirection: DismissDirection.horizontal,
+      messageText: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SvgPicture.asset(
+            "assets/icons/info-circle.svg",
+            color: Colors.white,
+          ),
+          SizedBox(width: 16),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  margin: EdgeInsets.only(bottom: 4),
+                  child: Text(
+                    title ?? "error",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w500,
+                      fontFamily: 'poppins',
+                    ),
+                  ),
+                ),
+                Text(
+                  message ?? "Add your error message here",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      backgroundColor: AppColor.error,
+      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+      margin: EdgeInsets.symmetric(horizontal: 16),
+      borderRadius: 8,
+      snackPosition: SnackPosition.TOP,
+    );
+  }
+
+  static successSnackbar(String? title, String? message) {
+    Get.rawSnackbar(
+      duration: Duration(seconds: 4),
+      dismissDirection: DismissDirection.horizontal,
+      messageText: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SvgPicture.asset(
+            "assets/icons/success-circle.svg",
+            color: Colors.white,
+          ),
+          SizedBox(width: 16),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  margin: EdgeInsets.only(bottom: 4),
+                  child: Text(
+                    title ?? "Success",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w500,
+                      fontFamily: 'poppins',
+                    ),
+                  ),
+                ),
+                Text(
+                  message ?? "Add your success message here",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      backgroundColor: AppColor.success,
+      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+      margin: EdgeInsets.symmetric(horizontal: 16),
+      borderRadius: 8,
+      snackPosition: SnackPosition.TOP,
+    );
+  }
+
+  static warningSnackbar(String? title, String? message) {
+    Get.rawSnackbar(
+      duration: Duration(seconds: 4),
+      dismissDirection: DismissDirection.horizontal,
+      messageText: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SvgPicture.asset(
+            "assets/icons/warning-circle.svg",
+            color: Colors.white,
+          ),
+          SizedBox(width: 16),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  margin: EdgeInsets.only(bottom: 4),
+                  child: Text(
+                    title ?? "Warning",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w500,
+                      fontFamily: 'poppins',
+                    ),
+                  ),
+                ),
+                Text(
+                  message ?? "Add your warning message here",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      backgroundColor: AppColor.warning,
+      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+      margin: EdgeInsets.symmetric(horizontal: 16),
+      borderRadius: 8,
+      snackPosition: SnackPosition.TOP,
+    );
+  }
+}
